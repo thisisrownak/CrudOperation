75079c30ca8eabb4c0012b185c3ab71b362536b3
Rownak <Rownak@DESKTOP-37NU34P>
2022-10-13 11:46:59 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coreMVC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
